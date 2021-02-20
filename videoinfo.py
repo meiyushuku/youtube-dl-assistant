dc26504ec174237573d921d6d795133c4c3733af
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-20 19:26:38 +0800

    Update videoinfo.py

--- videoinfo.py
+++ videoinfo.py
@@ -14,27 +14,45 @@ youtube = build(
     )
 
 def get_published_at(video_id):
-    published_at = youtube.videos().list(part = "snippet", id = video_id).execute()["items"][0]["snippet"]["publishedAt"]
+    published_at = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["publishedAt"]
     return published_at
 
 def get_channel_id(video_id):
-    channel_id = youtube.videos().list(part = "snippet", id = video_id).execute()["items"][0]["snippet"]["channelId"]
+    channel_id = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["channelId"]
     return channel_id
 
 def get_title(video_id):
-    title = youtube.videos().list(part = "snippet", id = video_id).execute()["items"][0]["snippet"]["title"]
+    title = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["title"]
     return title
 
 def get_description(video_id):
-    description = youtube.videos().list(part = "snippet", id = video_id).execute()["items"][0]["snippet"]["description"]
+    description = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["description"]
     return description
 
 def get_channel_title(video_id):
-    channel_title = youtube.videos().list(part = "snippet", id = video_id).execute()["items"][0]["snippet"]["channelTitle"]
+    channel_title = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["channelTitle"]
     return channel_title
 
 def get_duration(video_id):
-    duration = youtube.videos().list(part = "contentDetails", id = video_id).execute()["items"][0]["contentDetails"]["duration"]
+    duration = youtube.videos().list(
+        part = "contentDetails",
+        id = video_id
+        ).execute()["items"][0]["contentDetails"]["duration"]
     return duration
 
 '''
@@ -50,7 +68,5 @@ print("")
 print(get_channel_title(video_id))
 print("")
 print(get_duration(video_id))
-print("")
-print(error_snippet)
 print("")
 '''
