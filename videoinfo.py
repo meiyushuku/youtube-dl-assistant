d0df5bcc10a2f6fc5546929afc8e8c94b9c5af13
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-21 06:35:12 +0800

    Update videoinfo.py

--- videoinfo.py
+++ videoinfo.py
@@ -14,6 +14,45 @@ youtube = build(
     developerKey=YOUTUBE_API_KEY
     )
 
+def get_video_info(video_id):
+    video_info_list = list()
+    video_info_list.clear()
+    channel_title = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["channelTitle"]
+    channel_id = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["channelId"]   
+    published_at = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["publishedAt"]
+    title = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["title"]
+    description = youtube.videos().list(
+        part = "snippet",
+        id = video_id
+        ).execute()["items"][0]["snippet"]["description"]
+    duration = youtube.videos().list(
+        part = "contentDetails",
+        id = video_id
+        ).execute()["items"][0]["contentDetails"]["duration"]
+    video_info_list.append(channel_title) # 0
+    video_info_list.append(channel_id) # 1
+    video_info_list.append(published_at) # 2
+    video_info_list.append(title) # 3
+    video_info_list.append(description) # 4
+    video_info_list.append(duration) # 5
+    return video_info_list
+
+
+
+
+'''
 def get_published_at(video_id):
     published_at = youtube.videos().list(
         part = "snippet",
@@ -55,7 +94,7 @@ def get_duration(video_id):
         id = video_id
         ).execute()["items"][0]["contentDetails"]["duration"]
     return duration
-
+'''
 '''
 video_id = "J9zp5D6JddI"
 print(get_published_at(video_id))
