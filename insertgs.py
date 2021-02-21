f514285bd20d8a8de564bea82b7574067a4f570e
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-21 16:40:21 +0800

    Update insertgs.py

--- insertgs.py
+++ insertgs.py
@@ -18,19 +18,18 @@ SCOPE = "https://spreadsheets.google.com/feeds"
 credentials = ServiceAccountCredentials.from_json_keyfile_name(SHEET_KEY_FILE, SCOPE)
 client = gspread.authorize(credentials)
 
-def insert(video_id, file_name_abs):
+def insert(video_info_list, video_id, file_name_abs): # Catch video_info_list from rename.
     insert_list = list()
     insert_list.append("YT") # site
-    insert_list.append(videoinfo.get_video_info(video_id)[1]) # channelId
-    insert_list.append(videoinfo.get_video_info(video_id)[2]) # publishedAt
+    insert_list.append(video_info_list[1]) # channelId
+    insert_list.append(video_info_list[2]) # publishedAt
     insert_list.append(video_id) # videoId
-    insert_list.append(videoinfo.get_video_info(video_id)[3]) # title
-    insert_list.append(json.dumps(videoinfo.get_video_info(video_id)[4], ensure_ascii = False)) # description
+    insert_list.append(video_info_list[3]) # title
+    insert_list.append(json.dumps(video_info_list[4], ensure_ascii = False)) # description
     insert_list.append("") # customDescription
-    insert_list.append(videoinfo.get_video_info(video_id)[5]) # duration
+    insert_list.append(video_info_list[5]) # duration
     insert_list.append(USER) # user
     insert_list.append(time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(time.time()))) # lastUpdate
     insert_list.append(re.sub("[.]", "", os.path.splitext(file_name_abs)[1])) # extension
     sheet = client.open_by_key(SHEET_ID).sheet1
     sheet.append_row(insert_list, table_range = "A:A")
-    insert_list.clear()
