5605d6efee648586f76416cf3daa4d6c7983bd2b
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-22 03:16:38 +0800

    Update insertgs.py

--- insertgs.py
+++ insertgs.py
@@ -4,6 +4,7 @@ import time
 import json
 import codecs
 import gspread # pip install gspread
+#import requests # pip3 install requests2
 from oauth2client.service_account import ServiceAccountCredentials # pip install oauth2client
 
 with codecs.open("config.json", "r", "utf-8") as json_file:
@@ -16,6 +17,17 @@ SCOPE = "https://spreadsheets.google.com/feeds"
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(SHEET_KEY_FILE, SCOPE)
 client = gspread.authorize(credentials)
+
+sheet = client.open_by_key(SHEET_ID).sheet1
+
+def exists(video_id):
+    video_exists = 0
+    try:
+        if sheet.find(video_id) != None:
+            video_exists = 1
+    except:
+        pass
+    return video_exists
 
 def insert(video_info_list, video_id, file_name_abs): # Catch video_info_list from rename.
     insert_list = list()
@@ -32,3 +44,33 @@ def insert(video_info_list, video_id, file_name_abs): # Catch video_info_list fr
     insert_list.append(re.sub("[.]", "", os.path.splitext(file_name_abs)[1])) # extension
     sheet = client.open_by_key(SHEET_ID).sheet1
     sheet.append_row(insert_list, table_range = "A:A")
+
+'''
+DATABASE_API_URL = config["confidentials"]["google"]["databeseApiUrl"]
+
+def exists(video_id):
+    video_info_url = DATABASE_API_URL + "?method=getVideoInfoByVideoId&site=YT&videoId=" + video_id
+    response = requests.get(video_info_url)
+    if json.loads(response.text):
+        video_exists = 1
+    else:
+        video_exists = 0
+    return video_exists
+
+def insert(video_info_list, video_id, file_name_abs): # Catch video_info_list from rename.
+    video_info_dict = dict()
+    video_info_dict["method"] = "insertVideoInfo"
+    video_info_dict["site"] = "YT"
+    video_info_dict["channelId"] = video_info_list[1]
+    video_info_dict["publishedAt"] = video_info_list[2]
+    video_info_dict["videoId"] = video_id
+    video_info_dict["title"] = video_info_list[3]
+    video_info_dict["description"] = video_info_list[4]
+    video_info_dict["customDescription"] = ""
+    video_info_dict["duration"] = video_info_list[5]
+    video_info_dict["user"] = USER
+    video_info_dict["extension"] = re.sub("[.]", "", os.path.splitext(file_name_abs)[1])
+    video_info_dict["ignoreIfExist"] = True
+    video_info_dict_json = json.dumps(video_info_dict, ensure_ascii = False)
+    response = requests.post(DATABASE_API_URL, json = video_info_dict)
+'''
