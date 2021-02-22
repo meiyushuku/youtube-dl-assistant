0f4b8dc233b407a498440e27f13c00ca04000750
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-23 04:42:14 +0800

    Update tester.py

--- test/tester.py
+++ test/tester.py
@@ -1,47 +1,9 @@
-import sys
-import threading
-import time
-from rename import rename_exe
+from doc import tool
 
-VERSION = "0.9.0"
-print("youtube-dl-assistant-private")
-print("Version %s (2021) Developed by Meiyu Shuku" % VERSION)
-print("")
+config = tool.json_reader("doc/config.json")
 
-def switch():
-    global monit_mission
-    input_menu_orig = input("Type here >>> ")
-    input_menu = input_menu_orig.strip()
-    if input_menu == "rename":
-        rename_exe()
-    elif input_menu == "monit -on":
-        monit_mission = 1
-        thread.start()
-        return switch()
-    elif input_menu == "":
-        print("Not entered.")
-        return switch()
-    else:
-        print("Command is not defined.")
-        return switch()
+path = config["general"]["workDir"]
+isvideo = config["general"]["isVideo"]
 
-def monit():
-    while monit_mission:
-        rename_exe()
-        time.sleep(1)
-
-thread = threading.Thread(target = monit)
-
-
-controller = 1
-while controller:
-    switch()
-
-    print('Task is completed. Type "menu" to return to menu or exit by any other.')
-    input_end_orig = input("Type here >>> ")
-    input_end = input_end_orig.strip()
-    if input_end == "menu":
-        print("Returned to menu.")
-        pass
-    else:
-        sys.exit()
+print(path)
+print(isvideo)
