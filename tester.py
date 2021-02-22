53da96b73f81d1e8560f8cf551e5ee12c29be4de
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-22 13:42:58 +0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,9 +1,32 @@
-import tkinter
+import threading
+import time
+
+import sys
+import json
+import codecs
+from mttkinter import mtTkinter as tk # pip install mttkinter
 from tkinter import filedialog
 
-root = tkinter.Tk()
-root.withdraw()
+def read_json(json_file_name):
+	with codecs.open(json_file_name, "rb", "utf-8") as json_file:
+		json_data = json.load(json_file)
+	return json_data
 
-file_path = filedialog.askdirectory()
+def write_json(json_file_name, config):
+	with codecs.open(json_file_name, "w", "utf-8") as json_file:
+		json.dump(config, json_file, ensure_ascii = False, indent = 4)
 
-print(file_path)
+
+def chgdir():
+    input_chgdir_orig = input("Type here >>> ")
+    input_chgdir = input_chgdir_orig.strip()
+    json_file_name = "configtest.json"
+    config = read_json(json_file_name)
+    config["general"]["workDir"] = input_chgdir
+    write_json(json_file_name, config)
+
+
+
+
+
+input()
