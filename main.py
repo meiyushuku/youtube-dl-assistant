58d52a7607282e832d88126872f84f54b912dced
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-22 14:00:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,33 +13,12 @@ def switch():
     input_menu = input_menu_orig.strip()
     if input_menu == "rename":
         rename_exe()
-    elif input_menu == "setting":
-        chgdir()
-        return switch()
     elif input_menu == "":
         print("Not entered.")
         return switch()
     else:
         print("Command is not defined.")
         return switch()
-
-def chgdir():
-    input_chgdir_orig = input("Type here >>> ")
-    input_chgdir = input_chgdir_orig.strip()
-    json_file_name = "config.json"
-    config = read_json(json_file_name)
-    config["general"]["workDir"] = input_chgdir
-    write_json(json_file_name, config)
-    print("Returned to menu.")
-
-def read_json(json_file_name):
-	with codecs.open(json_file_name, "rb", "utf-8") as json_file:
-		json_data = json.load(json_file)
-	return json_data
-
-def write_json(json_file_name, config):
-	with codecs.open(json_file_name, "w", "utf-8") as json_file:
-		json.dump(config, json_file, ensure_ascii = False, indent = 4)
 
 controller = 1
 while controller:
