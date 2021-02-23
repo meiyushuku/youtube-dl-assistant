4fadfc3f4ede28ff74be61651740946ca6a4d81c
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-23 18:31:45 +0800

    Update common.py

--- ytdlasst/common.py
+++ ytdlasst/common.py
@@ -1,3 +1,4 @@
+import os
 import json
 import codecs
 
@@ -9,3 +10,7 @@ def json_reader(json_file_name):
 def json_writer(json_file_name, content):
 	with codecs.open(json_file_name, "w", "utf-8") as json_file:
 		json.dump(content, json_file, ensure_ascii = False, indent = 4)
+
+def make_dir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
