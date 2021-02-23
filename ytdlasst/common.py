12c7f3b789b77470955ea543daa5929983aa643c
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-23 18:42:36 +0800

    Update common.py

--- ytdlasst/common.py
+++ ytdlasst/common.py
@@ -2,12 +2,12 @@ import os
 import json
 import codecs
 
-def json_reader(json_file_name):
+def read_json(json_file_name):
 	with codecs.open(json_file_name, "rb", "utf-8") as json_file:
 		json_data = json.load(json_file)
 	return json_data
 
-def json_writer(json_file_name, content):
+def write_json(json_file_name, content):
 	with codecs.open(json_file_name, "w", "utf-8") as json_file:
 		json.dump(content, json_file, ensure_ascii = False, indent = 4)
 
