0e1cff2aa23a9495b5b1992637915ac9055f768a
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-24 08:51:04 +0800

    Update main.py

--- ytdlasst/main.py
+++ ytdlasst/main.py
@@ -8,32 +8,19 @@ print("youtube-dl-assistant-private")
 print("Version %s (2021) Developed by Meiyu Shuku" % VERSION)
 print("")
 
+'''
 def switch():
     input_menu_orig = input("Type here >>> ")
     input_menu = input_menu_orig.strip()
-    if input_menu == "test":
-        print("test")
-        return switch()
+    if input_menu == "exit":
+        sys.exit()
     elif input_menu == "":
         print("Not entered.")
         return switch()
     else:
         print("Command is not defined.")
         return switch()
-
-def fproc_bg():
-    while True:
-        main()
+'''
 
 while True:
-    thread = threading.Thread(target = fproc_bg)
-    thread.start()
-    switch()
-    print('Task is completed. Type "menu" to return to menu or exit by any other.')
-    input_end_orig = input("Type here >>> ")
-    input_end = input_end_orig.strip()
-    if input_end == "menu":
-        print("Returned to menu.")
-        pass
-    else:
-        sys.exit()
+    main()
