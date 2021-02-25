7b818b39d9465dc505224b621dfa9059eba03d71
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-25 13:47:14 +0800

    Update test.py

--- ytdlasst/test.py
+++ ytdlasst/test.py
@@ -1,4 +1,13 @@
-ignore_temp = {}
-ignore_temp["ignore"] = ""
+import requests
+import json
 
-print(ignore_temp)
+def test(url):
+    response = requests.get(url)
+    if json.loads(response.text)["error"]["errors"][0]["reason"] != "badRequest":
+        test = 1 # 1
+    else:
+        test = 0 # 0
+    return test
+
+url = "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyAVKnfeGxZe3fMlpvNrlkrhD8hEs4DU6j"
+print(test(url))
