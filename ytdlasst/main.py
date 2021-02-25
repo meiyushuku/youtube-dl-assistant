8200495cfabe29a9f341f4547b0e5d762412da52
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-25 13:47:11 +0800

    Update main.py

--- ytdlasst/main.py
+++ ytdlasst/main.py
@@ -1,8 +1,17 @@
+VERSION = "0.9.0"
+print("youtube-dl-assistant")
+print("Version %s (2021) Developed by Meiyu Shuku" % VERSION)
+print("")
+
 import threading
 import os
 import sys
 import time
+import json
 import common
+
+from getyt import _getyt_init
+
 from fileproc import main
 
 def ignore_init():
@@ -10,21 +19,23 @@ def ignore_init():
     ignore_init["ignore"] = ""
     common.write_json("doc/ignore.json", ignore_init)
 
-VERSION = "0.9.0"
-print("youtube-dl-assistant")
-print("Version %s (2021) Developed by Meiyu Shuku" % VERSION)
-print("")
-print("Loading...")
-print("")
-
 config = common.read_json("doc/config.json")
 work_dir = config["general"]["workDir"]
 is_video = config["general"]["isVideo"]
 
 if os.path.isdir(work_dir):
-    ignore_init()
-    while True:
-        main(work_dir, is_video)
+    if os.path.isfile("doc/confidentials.json"):
+        with codecs.open("doc/confidentials.json", "rb", "utf-8") as json_file:
+            confidentials = json.load(json_file)
+        if _getyt_init(confidentials) == True:
+            if _insertgs_init(confidentials) == True:
+                ignore_init()
+                while True:
+                main(work_dir, is_video)
+    else:
+        print('"confidentials.json" not found.')
+        input()
 else:
-    print("dir_error")
+    print("Working path must be a directory.")
     input()
+
