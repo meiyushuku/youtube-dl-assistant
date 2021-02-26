8a3d5c24d91703970609867a91ddba6af24d5121
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-02-27 07:02:49 +0800

    Update insertrdb.py

--- ytdlasst/insertrdb.py
+++ ytdlasst/insertrdb.py
@@ -5,22 +5,22 @@ import common
 import mysql.connector as mydb # pip install mysql-connector-python
 
 def connect():
-  # DB接続に失敗した場合の例外対策
-  try:
-    resconn = mydb.connect(
-      host = confidentials["insertRdb"]["host"],
-      port = confidentials["insertRdb"]["port"],
-      user = confidentials["insertRdb"]["user"],
-      password = confidentials["insertRdb"]["password"],
-      database = confidentials["insertRdb"]["database"]
-    )
-    print("conn successfully")
-  except Exception as e:
-    print('[DB Connection Error]', e)
-    sys.exit(1) # プログラムをエラー終了
-  # 接続が切れた場合に自動的に再接続する
-  resconn.ping(reconnect = True)
-  return resconn
+    # DB接続に失敗した場合の例外対策
+    try:
+        resconn = mydb.connect(
+            host = confidentials["insertRdb"]["host"],
+            port = confidentials["insertRdb"]["port"],
+            user = confidentials["insertRdb"]["user"],
+            password = confidentials["insertRdb"]["password"],
+            database = confidentials["insertRdb"]["database"]
+        )
+        print("conn successfully")
+    except Exception as e:
+        print('[DB Connection Error]', e)
+        sys.exit(1) # プログラムをエラー終了
+    # 接続が切れた場合に自動的に再接続する
+    resconn.ping(reconnect = True)
+    return resconn
 
 def insert_data(_conn, _query):
     # INSERTのクエリかどうかを判別
@@ -36,10 +36,37 @@ def insert_data(_conn, _query):
         _conn.rollback() # ロールバックする
         sys.exit(1)
 
-confidentials = common.read_json("doc/confidentials.json")
+def insert_channel_info():
+    channel_id = "1hgh786868"
+    insert_sql = '''INSERT INTO channelInfo(
+        site,
+        channelId,
+        channelName,
+        isOfficial,
+        count
+        ) VALUES ("YT", "{:s}", "123", "1", "20")'''.format(
+            channel_id
+            )
+    insert_data(conn, insert_sql)
 
+def insert_video_info():
+    insert_sql = '''INSERT INTO videoInfo(
+        site,
+        channelId,
+        publishedAt,
+        videoId,
+        title,
+        description,
+        customDescription,
+        duration,
+        user,
+        lastUpdate,
+        extension
+        ) VALUES ("{:s}", "{:s}", "{:s}", "{:s}", "{:s}")'''.format(
+            123
+            )
+    insert_data(conn, insert_sql)
+
+confidentials = common.read_json("doc/confidentials.json")
 conn = connect()
-channel_id = "125499995645377"
-insert_sql = '''INSERT INTO channelInfo(site,channelId,channelName,isOfficial,count
-) VALUES ("YT","{:s}","123","1","20")'''.format(channel_id)
-insert_data(conn, insert_sql)
+insert_channel_info()
